4568ed0f576fe9168aa6557360178332314cc813
huangshuheng <3444104242@qq.com>
2026-04-17 14:37:34 +0800

    fix: 🐛  el.setAttribute(key, value)

--- packages/runtime-dom/src/modules/patchAttr.ts
+++ packages/runtime-dom/src/modules/patchAttr.ts
@@ -1,7 +1,13 @@
-export function patchAttr(el, key, value) {
-  if (value === null) {
+/**
+ * 处理属性
+ * @param el 元素
+ * @param key 属性名
+ * @param value 属性值
+ */
+export function patchAttr(el: any, key: any, value: any) {
+  if (value == null) {
     el.removeAttribute(key)
   } else {
-    el.setAttribute(key)
+    el.setAttribute(key, value)
   }
 }
